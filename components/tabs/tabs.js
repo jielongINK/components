ce68d8ffe87602fad487cd47f74deba094cf5062
jielongINK <heyliudiao@gmail.com>
2017-11-29 21:54:39 +0800

    完成tabs组件

---
+++ components/tabs/tabs.js
@@ -0,0 +1,20 @@
+var Tabs = function() {
+    var o = {
+        nav: "tab-nav",
+        panel: "tab-panel",
+    }
+    o.init = function() {
+        $("[data-role='tab-panel'] li").uniqueClass(0, 'active')
+        $("[data-role='tab-nav'] li").on('click', function (event) {
+            var index = $().index(event.target)
+            $("[data-role='tab-panel'] li").uniqueClass(index, 'active')
+        })
+        
+        return this
+    }
+    
+
+    return o
+}
+
+new Tabs().init()
---
+++ tools/dom.js
@@ -0,0 +1,29 @@
+const $ = function(selector) {
+    var o = {}
+    o.element = document.querySelectorAll(selector)
+
+    o.index = function(ele) {
+        var siblings = ele.parentNode.children
+        return Array.prototype.indexOf.call(siblings,ele)
+    }
+
+    o.uniqueClass = function(index, className) {
+        o.element.forEach((obj,ind) => {
+            if(ind !== index){
+                obj.classList.remove(className)
+            }else{
+                obj.classList.add(className)
+            }
+        })
+    }
+
+    o.on = function(eventType, func) {
+        o.element.forEach((ele, index) => ele.addEventListener(eventType, func))
+
+        return this
+    }
+
+    return o
+}
+
+var log = console.log.bind(console)
