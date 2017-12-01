6127b47b09b927f56b4fa0d035eb795b405911b3
jielongINK <heyliudiao@gmail.com>
2017-12-01 14:07:38 +0800

    代码优化

--- components/carousel/carousel.js
+++ components/carousel/carousel.js
@@ -15,30 +15,10 @@ var Carousel = function() {
     }
     o.bindEvent = function() {
         o.next.on("click",function() {
-            var oldP = parseInt(o.picList.getStyle("left"))
-            var newP = oldP - o.picWidth
-            o.current++
-            if(Math.abs(newP) == o.picWidth * o.picNum){
-                newP = "0px"
-                o.current = 0
-            }else{
-                newP += "px"
-            }
-            o.picList.setStyle("left", newP)
-            $(".min-button li").uniqueClass(o.current, "current")
+            o.changePic("next")
         })
         o.prev.on("click", function () {
-            var oldP = parseInt(o.picList.getStyle("left"))
-            var newP = oldP + o.picWidth
-            o.current --
-            if (newP == o.picWidth) {
-                newP = o.picWidth * (o.picNum - 1) * -1 + "px"
-                o.current = o.picNum - 1
-            } else {
-                newP += "px"
-            }
-            o.picList.setStyle("left", newP)
-            $(".min-button li").uniqueClass(o.current, "current")
+            o.changePic("prev")
         })
         o.minButton.on("click", function(event) {
             event.stopPropagation()
@@ -61,6 +41,30 @@ var Carousel = function() {
             var li = o.createLi(i)
             o.minButton.getEle().appendChild(li)            
         }
+    }
+    o.changePic = function(action) {
+        var oldP = parseInt(o.picList.getStyle("left"))
+        if(action == "prev"){
+            var newP = oldP + o.picWidth
+            o.current--
+            if (newP == o.picWidth) {
+                newP = o.picWidth * (o.picNum - 1) * -1 + "px"
+                o.current = o.picNum - 1
+            } else {
+                newP += "px"
+            }
+        }else if(action == "next"){
+            var newP = oldP - o.picWidth
+            o.current++
+            if (Math.abs(newP) == o.picWidth * o.picNum) {
+                newP = "0px"
+                o.current = 0
+            } else {
+                newP += "px"
+            }
+        }
+        o.picList.setStyle("left", newP)
+        $(".min-button li").uniqueClass(o.current, "current")
     }
 
     return o
